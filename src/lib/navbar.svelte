1769fdef232362e7859bb43c1e487f476221691e
HenryBGit <henrybrookland8@gmail.com>
2023-07-19 14:34:47 +1200

    file: add navbar code and file

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,34 @@
+<nav>
+    <ul>
+        <li><a href="/"> Neoni</a></li>
+        <li><a href="/artists-Songs">Songs</a></li>
+        <li><a href="/about"> About Neoni</a></li>
+    </ul>
+</nav>
+<style>   
+nav {
+    background-color: blue;
+}
+nav ul {
+    display: flex;
+	justify-content: center;
+	text-decoration: none;
+	list-style-type: none;
+    line-height: 45px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+}
+nav li a {
+    padding: 15px;
+    font-size: 30px;
+	color: #ececec;
+	text-align: center;
+    justify-items: center;
+	text-decoration: none;
+	font-family: 'Cinzel', serif;
+}
+nav li a:hover {
+    background-color: #ec008c;
+}
+</style>
